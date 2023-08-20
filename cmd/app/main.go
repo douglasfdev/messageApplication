4a45222a551d241eeef15ff3d57892e6a6683cc7
Douglas Pereira Junior <douglaspereirajunior@MacBook-Pro-de-Douglas.local>
2023-08-19 23:16:36 -0300

    fix(db) fix the string connection

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -15,7 +15,7 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root:root@tcp(host:docker.internal:3306/products)")
+	db, err := sql.Open("mysql", "root:root@tcp(host:docker.internal:3306)/products")
 
 	if err != nil {
 		panic(err)
